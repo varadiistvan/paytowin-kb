380d94b44e94a18764d7bf4d0031e0483cddd7c5
varadiistvan <varadiista.1@gmail.com>
2024-04-21 21:23:13 +0200

    changes from laptop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,20 +8,28 @@
 // import { PaytowinClientImpl, PaytowinServiceName, PlayersRequest, PlayersResponse } from './paytowin';
 // import { Observable } from 'rxjs';
 
-import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport"
-import { PayToWinClient } from "./generated/paytowin.client"
-import { DatalessEffect, DiamondTool, EffectRequest, MinecraftEntity, MinecraftMaterial, PlayersRequest, PotionName, SpawnEntity } from "./generated/paytowin";
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+import { PayToWinClient } from "./generated/paytowin.client";
+import {
+  DatalessEffect,
+  DiamondTool,
+  EffectRequest,
+  MinecraftEntity,
+  MinecraftMaterial,
+  PlayersRequest,
+  PotionName,
+  SpawnEntity,
+} from "./generated/paytowin";
 import { useEffect, useState } from "react";
-import './index.css';
+import "./index.css";
 import ReactSelect from "react-select";
 
 function App() {
-
   const transport: GrpcWebFetchTransport = new GrpcWebFetchTransport({
     baseUrl: "http://localhost:8080",
     meta: {
-      "password": "assword",
-    }
+      password: "assword",
+    },
   });
 
   const client: PayToWinClient = new PayToWinClient(transport);
@@ -42,29 +50,29 @@ function App() {
 
   const [itemAmount, selectItemAmount] = useState<number>();
 
-  const [selectedEntityEffect, selectEntityEffect] = useState<MinecraftEntity>();
+  const [selectedEntityEffect, selectEntityEffect] =
+    useState<MinecraftEntity>();
 
   const [entityAmount, selectEntityAmount] = useState<number>();
 
   const [requester, setRequester] = useState<string>();
-  
-  const [effectType, changeEffectType] = useState<EffectRequest["effect"]["oneofKind"]>(undefined);
+
+  const [effectType, changeEffectType] =
+    useState<EffectRequest["effect"]["oneofKind"]>(undefined);
 
   useEffect(() => {
     const playersResponse = client.getPlayers(PlayersRequest.create());
 
     (async () => {
       try {
-          for await (const {players} of playersResponse.responses) {
-            console.log(players);
-            setPlayersOnline(players);
-          }
+        for await (const { players } of playersResponse.responses) {
+          console.log(players);
+          setPlayersOnline(players);
+        }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    })()
-
-    
+    })();
   }, []);
 
   const applyMiscEffect = (player: string, effect: DatalessEffect): void => {
@@ -72,26 +80,26 @@ function App() {
       EffectRequest.create({
         effect: {
           dataless: effect,
-          oneofKind: "dataless"
-        }, 
-          player: player,
-          requester: requester,
-        })
-    )
-  }
+          oneofKind: "dataless",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
   const applyToolEffect = (player: string, effect: DiamondTool): void => {
     client.applyEffect(
       EffectRequest.create({
         effect: {
           tool: effect,
-          oneofKind: "tool"
-        }, 
-          player: player,
-          requester: requester
-        })
-    )
-  }
+          oneofKind: "tool",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
   const applyPotionEffect = (player: string, effect: PotionName): void => {
     client.applyEffect(
@@ -100,71 +108,78 @@ function App() {
           potion: {
             name: effect,
             duration: 60,
-            amplifier: 3
-          }, 
-          oneofKind: "potion"
-        }, 
-          player: player,
-          requester: requester
-        })
-    )
-  }
+            amplifier: 3,
+          },
+          oneofKind: "potion",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
-  const applyItemEffect = (player: string, effect: MinecraftMaterial, amount: number): void => {
+  const applyItemEffect = (
+    player: string,
+    effect: MinecraftMaterial,
+    amount: number,
+  ): void => {
     client.applyEffect(
       EffectRequest.create({
         effect: {
           item: {
             itemName: effect,
             amount: amount,
-          }, 
-          oneofKind: "item"
-        }, 
-          player: player,
-          requester: requester
-        })
-    )
-  }
+          },
+          oneofKind: "item",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
-  const applyEntityEffect = (player: string, effect: MinecraftEntity, amount: number): void => {
+  const applyEntityEffect = (
+    player: string,
+    effect: MinecraftEntity,
+    amount: number,
+  ): void => {
     client.applyEffect(
       EffectRequest.create({
         effect: {
           spawnEntity: {
             entity: effect,
             amount: amount,
-          }, 
-          oneofKind: "spawnEntity"
-        }, 
-          player: player,
-          requester: requester
-        })
-    )
-  }
+          },
+          oneofKind: "spawnEntity",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
   const sendToHeaven = (player: string): void => {
     client.applyEffect(
-      EffectRequest.create(
-        {
-          effect: {
-            potion: {
-              name: PotionName.LEVITATION,
-              duration: 1,
-              amplifier: 100,
-            },
-            oneofKind: "potion"
+      EffectRequest.create({
+        effect: {
+          potion: {
+            name: PotionName.LEVITATION,
+            duration: 1,
+            amplifier: 100,
           },
-          player: player,
-          requester: requester
-        }
-      )
-    )
-  }
+          oneofKind: "potion",
+        },
+        player: player,
+        requester: requester,
+      }),
+    );
+  };
 
-  const applyEffect = ():void => {
+  const applyEffect = (): void => {
     if (selectedPlayer == "") return;
     if (selectedEffect == "Send to heaven") {
       sendToHeaven(selectedPlayer);
+      alert("success");
       return;
     }
     if (effectType == "potion" && selectedPotionEffect != undefined) {
@@ -179,181 +194,225 @@ function App() {
     if (effectType == "item" && itemAmount && selectedItemEffect != undefined) {
       applyItemEffect(selectedPlayer, selectedItemEffect, itemAmount);
     }
-    if (effectType == "spawnEntity" && entityAmount && selectedEntityEffect != undefined) {
+    if (
+      effectType == "spawnEntity" &&
+      entityAmount &&
+      selectedEntityEffect != undefined
+    ) {
       applyEntityEffect(selectedPlayer, selectedEntityEffect, entityAmount);
     }
-  }
 
-  const listOfPotionEffects = (Object.entries(PotionName))
-  .filter(([, value]) => typeof value === 'number')
-  .map(([key, value]) => ({ 
-    value: value as PotionName, 
-    label: key, 
-  }));
+    alert("success");
+  };
 
-  const listOfMiscEffects = (Object.entries(DatalessEffect))
-  .filter(([, value]) => typeof value === 'number')
-  .map(([key, value]) => ({ 
-    value: value as DatalessEffect, 
-    label: key, 
-  }));
+  const listOfPotionEffects = Object.entries(PotionName)
+    .filter(([, value]) => typeof value === "number")
+    .map(([key, value]) => ({
+      value: value as PotionName,
+      label: key,
+    }));
 
-  const listOfToolEffects = (Object.entries(DiamondTool))
-  .filter(([, value]) => typeof value === 'number')
-  .map(([key, value]) => ({ 
-    value: value as DiamondTool, 
-    label: key, 
-  }));
+  const listOfMiscEffects = Object.entries(DatalessEffect)
+    .filter(([, value]) => typeof value === "number")
+    .map(([key, value]) => ({
+      value: value as DatalessEffect,
+      label: key,
+    }));
 
-  const listOfItemEffects = (Object.entries(MinecraftMaterial))
-  .filter(([, value]) => typeof value === 'number')
-  .map(([key, value]) => ({ 
-    value: value as MinecraftMaterial, 
-    label: key, 
-  }));
+  const listOfToolEffects = Object.entries(DiamondTool)
+    .filter(([, value]) => typeof value === "number")
+    .map(([key, value]) => ({
+      value: value as DiamondTool,
+      label: key,
+    }));
 
-  const itemAmountList = [{ value: 1, label: 1 }, { value: 4, label: 4 }, { value: 16, label: 16 }, { value: 32, label: 32 }, { value: 64, label: 64 }];
+  const listOfItemEffects = Object.entries(MinecraftMaterial)
+    .filter(([, value]) => typeof value === "number")
+    .map(([key, value]) => ({
+      value: value as MinecraftMaterial,
+      label: key,
+    }));
 
-  const listOfEntityEffects = (Object.entries(MinecraftEntity))
-  .filter(([, value]) => typeof value === 'number')
-  .map(([key, value]) => ({ 
-    value: value as MinecraftEntity, 
-    label: key, 
-  }));
+  const itemAmountList = [
+    { value: 1, label: 1 },
+    { value: 4, label: 4 },
+    { value: 16, label: 16 },
+    { value: 32, label: 32 },
+    { value: 64, label: 64 },
+  ];
 
-  const entityAmountList = [{ value: 1, label: 1 }, { value: 2, label: 2 }, { value: 4, label: 4 }, { value: 8, label: 8 }, { value: 16, label: 16 }];
+  const listOfEntityEffects = Object.entries(MinecraftEntity)
+    .filter(([, value]) => typeof value === "number")
+    .map(([key, value]) => ({
+      value: value as MinecraftEntity,
+      label: key,
+    }));
 
-  return (<>
-    <div className="text-xl font-bold m-2">Koornbeurs Minecraft LAN</div>
-    <div className="m-2">
-      Requester: <input className="outline" type="text" onChange={(event) => setRequester(event.target.value)}></input>
-    </div>
-    <div className="m-2">
-      Selected player: {selectedPlayer == "" ? "None" : <div className="font-bold">{selectedPlayer}</div>}
-    </div>
-    <div className="m-2">
-      Players online: 
-      <div className="flex flex-col sm:flex-row">
-        {playersOnline.map((player: string) => <div className="bg-blue-400 hover:bg-blue-500 p-2 m-2 max-w-fit" onClick={() => selectPlayer(player)}>{player}</div>)}
+  const entityAmountList = [
+    { value: 1, label: 1 },
+    { value: 2, label: 2 },
+    { value: 4, label: 4 },
+    { value: 8, label: 8 },
+    { value: 16, label: 16 },
+  ];
+
+  return (
+    <>
+      <div className="text-xl font-bold m-2">Koornbeurs Minecraft LAN</div>
+      <div className="m-2">
+        Requester:{" "}
+        <input
+          className="outline"
+          type="text"
+          onChange={(event) => setRequester(event.target.value)}
+        ></input>
       </div>
-    </div>
-    <div className="m-2">
-      Selected effect: {selectedEffect == "" ? "None" : <div className="font-bold">{selectedEffect}</div>}
-    </div>
-    <div className="m-2">
-      Effects: 
-      <div className="flex flex-col">
-        <div className="bg-blue-400 hover:bg-blue-500 p-2 m-2 max-w-fit" onClick={() => selectEffect("Send to heaven")}>
-          Send to heaven!
+      <div className="m-2">
+        Selected player:{" "}
+        {selectedPlayer == "" ? (
+          "None"
+        ) : (
+          <div className="font-bold">{selectedPlayer}</div>
+        )}
+      </div>
+      <div className="m-2">
+        Players online:
+        <div className="flex flex-col sm:flex-row">
+          {playersOnline.map((player: string) => (
+            <div
+              className="bg-blue-400 hover:bg-blue-500 p-2 m-2 max-w-fit"
+              onClick={() => selectPlayer(player)}
+            >
+              {player}
+            </div>
+          ))}
         </div>
-        <div>
-        Some misc effects:
-        <ReactSelect
-            options={listOfMiscEffects}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectMiscEffect(newValue.value)
-                selectEffect(newValue.label)
-                changeEffectType("dataless")
-              }
-            }}
+      </div>
+      <div className="m-2">
+        Selected effect:{" "}
+        {selectedEffect == "" ? (
+          "None"
+        ) : (
+          <div className="font-bold">{selectedEffect}</div>
+        )}
+      </div>
+      <div className="m-2">
+        Effects:
+        <div className="flex flex-col">
+          <div
+            className="bg-blue-400 hover:bg-blue-500 p-2 m-2 max-w-fit"
+            onClick={() => selectEffect("Send to heaven")}
+          >
+            Send to heaven!
+          </div>
+          <div>
+            Some misc effects:
+            <ReactSelect
+              options={listOfMiscEffects}
+              onChange={(newValue): void => {
+                if (newValue) {
+                  console.log(newValue.value);
+                  selectMiscEffect(newValue.value);
+                  selectEffect(newValue.label);
+                  changeEffectType("dataless");
+                }
+              }}
             />
-        </div>
-        <div>
-        Specific potion effects (mild, and 60 seconds):
-        <ReactSelect
-            options={listOfPotionEffects}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectPotionEffect(newValue.value)
-                selectEffect(newValue.label)
-                changeEffectType("potion")
-              }
-            }}
+          </div>
+          <div>
+            Specific potion effects (mild, and 60 seconds):
+            <ReactSelect
+              options={listOfPotionEffects}
+              onChange={(newValue): void => {
+                if (newValue) {
+                  console.log(newValue.value);
+                  selectPotionEffect(newValue.value);
+                  selectEffect(newValue.label);
+                  changeEffectType("potion");
+                }
+              }}
             />
           </div>
           <div>
             Give a diamond item:
             <ReactSelect
-            options={listOfToolEffects}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectToolEffect(newValue.value)
-                selectEffect(newValue.label)
-                changeEffectType("tool")
-              }
-            }}
+              options={listOfToolEffects}
+              onChange={(newValue): void => {
+                if (newValue) {
+                  console.log(newValue.value);
+                  selectToolEffect(newValue.value);
+                  selectEffect(newValue.label);
+                  changeEffectType("tool");
+                }
+              }}
             />
           </div>
           <div className="flex flex-row w-full m-2">
-            Give a certain amount of items:
-            Item:
+            Give a certain amount of items: Item:
             <div className="w-1/4 m-2">
-            <ReactSelect
-            options={listOfItemEffects}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectItemEffect(newValue.value)
-                selectEffect(newValue.label)
-                changeEffectType("item")
-              }
-            }}
-            />
+              <ReactSelect
+                options={listOfItemEffects}
+                onChange={(newValue): void => {
+                  if (newValue) {
+                    console.log(newValue.value);
+                    selectItemEffect(newValue.value);
+                    selectEffect(newValue.label);
+                    changeEffectType("item");
+                  }
+                }}
+              />
             </div>
             Amount:
             <div className="w-1/4 m-2">
-            <ReactSelect
-            options={itemAmountList}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectItemAmount(newValue.value)
-              }
-            }}
-            />
+              <ReactSelect
+                options={itemAmountList}
+                onChange={(newValue): void => {
+                  if (newValue) {
+                    console.log(newValue.value);
+                    selectItemAmount(newValue.value);
+                  }
+                }}
+              />
             </div>
           </div>
           <div className="flex flex-row">
-            Spawn a certain amount of mobs:
-            Mob:
+            Spawn a certain amount of mobs: Mob:
             <div className="w-1/4 m-2">
-            <ReactSelect
-            options={listOfEntityEffects}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectEntityEffect(newValue.value)
-                selectEffect(newValue.label)
-                changeEffectType("spawnEntity")
-              }
-            }}
-            />
+              <ReactSelect
+                options={listOfEntityEffects}
+                onChange={(newValue): void => {
+                  if (newValue) {
+                    console.log(newValue.value);
+                    selectEntityEffect(newValue.value);
+                    selectEffect(newValue.label);
+                    changeEffectType("spawnEntity");
+                  }
+                }}
+              />
             </div>
             Amount:
             <div className="w-1/4 m-2">
-            <ReactSelect
-            options={entityAmountList}
-            onChange={(newValue):void => {
-              if (newValue) {
-                console.log(newValue.value);
-                selectEntityAmount(newValue.value)
-              }
-            }}
-            />
+              <ReactSelect
+                options={entityAmountList}
+                onChange={(newValue): void => {
+                  if (newValue) {
+                    console.log(newValue.value);
+                    selectEntityAmount(newValue.value);
+                  }
+                }}
+              />
             </div>
           </div>
-        
+        </div>
       </div>
-    </div>
-    <div className="bg-red-400 hover:bg-red-500 p-2 m-2 max-w-fit" onClick={() => applyEffect()}>
-      Apply the effect!
-    </div>
-  </>);
-
+      <div
+        className="bg-red-400 hover:bg-red-500 p-2 m-2 max-w-fit"
+        onClick={() => applyEffect()}
+      >
+        Apply the effect!
+      </div>
+    </>
+  );
 
   // //const [count, setCount] = useState(0)
 
@@ -383,12 +442,12 @@ function App() {
   //       }
   //       resolve(res);
   //     };
-  
+
   //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   //     function passThrough(argument: any) {
   //       return argument;
   //     }
-  
+
   //     // Using passThrough as the serialize and deserialize functions
   //     client.makeUnaryRequest(path, passThrough, passThrough, data, resultCallback);
   //   });
@@ -434,7 +493,7 @@ function App() {
   // });
   // }
 
-  // const rpc = { 
+  // const rpc = {
   //   request: sendRequest,
   //   //clientStreamingRequest: sendRequest,
   //   serverStreamingRequest: sendServerStreamingRequest,
@@ -457,9 +516,7 @@ function App() {
   //     }
   //   })()
 
-    
   // }, [])
-
 
   // return (<>
   //   <div>
@@ -468,4 +525,4 @@ function App() {
   // </>);
 }
 
-export default App
+export default App;
